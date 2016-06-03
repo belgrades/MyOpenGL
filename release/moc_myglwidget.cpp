82d7f77713fd4e1f57511c3eb06b637db37d78f9
belgrades <fcremaldc@gmail.com>
2016-06-03 11:22:27 -0400

    junk files update

--- release/moc_myglwidget.cpp
+++ release/moc_myglwidget.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MyGLWidget_t {
-    QByteArrayData data[28];
-    char stringdata[456];
+    QByteArrayData data[21];
+    char stringdata[357];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -30,47 +30,37 @@ struct qt_meta_stringdata_MyGLWidget_t {
 static const qt_meta_stringdata_MyGLWidget_t qt_meta_stringdata_MyGLWidget = {
     {
 QT_MOC_LITERAL(0, 0, 10),
-QT_MOC_LITERAL(1, 11, 16),
-QT_MOC_LITERAL(2, 28, 0),
-QT_MOC_LITERAL(3, 29, 5),
-QT_MOC_LITERAL(4, 35, 16),
-QT_MOC_LITERAL(5, 52, 16),
-QT_MOC_LITERAL(6, 69, 13),
-QT_MOC_LITERAL(7, 83, 4),
-QT_MOC_LITERAL(8, 88, 27),
-QT_MOC_LITERAL(9, 116, 5),
-QT_MOC_LITERAL(10, 122, 19),
-QT_MOC_LITERAL(11, 142, 8),
-QT_MOC_LITERAL(12, 151, 12),
-QT_MOC_LITERAL(13, 164, 12),
-QT_MOC_LITERAL(14, 177, 12),
-QT_MOC_LITERAL(15, 190, 11),
-QT_MOC_LITERAL(16, 202, 25),
-QT_MOC_LITERAL(17, 228, 29),
-QT_MOC_LITERAL(18, 258, 30),
-QT_MOC_LITERAL(19, 289, 26),
-QT_MOC_LITERAL(20, 316, 24),
-QT_MOC_LITERAL(21, 341, 15),
-QT_MOC_LITERAL(22, 357, 6),
-QT_MOC_LITERAL(23, 364, 16),
-QT_MOC_LITERAL(24, 381, 17),
-QT_MOC_LITERAL(25, 399, 23),
-QT_MOC_LITERAL(26, 423, 14),
-QT_MOC_LITERAL(27, 438, 17)
+QT_MOC_LITERAL(1, 11, 27),
+QT_MOC_LITERAL(2, 39, 0),
+QT_MOC_LITERAL(3, 40, 5),
+QT_MOC_LITERAL(4, 46, 19),
+QT_MOC_LITERAL(5, 66, 8),
+QT_MOC_LITERAL(6, 75, 11),
+QT_MOC_LITERAL(7, 87, 5),
+QT_MOC_LITERAL(8, 93, 25),
+QT_MOC_LITERAL(9, 119, 29),
+QT_MOC_LITERAL(10, 149, 30),
+QT_MOC_LITERAL(11, 180, 26),
+QT_MOC_LITERAL(12, 207, 24),
+QT_MOC_LITERAL(13, 232, 9),
+QT_MOC_LITERAL(14, 242, 15),
+QT_MOC_LITERAL(15, 258, 6),
+QT_MOC_LITERAL(16, 265, 16),
+QT_MOC_LITERAL(17, 282, 17),
+QT_MOC_LITERAL(18, 300, 23),
+QT_MOC_LITERAL(19, 324, 14),
+QT_MOC_LITERAL(20, 339, 17)
     },
-    "MyGLWidget\0xRotationChanged\0\0angle\0"
-    "yRotationChanged\0zRotationChanged\0"
-    "pruebaChanged\0hola\0linea_button_pressed_signal\0"
-    "press\0progress_bar_signal\0progress\0"
-    "setXRotation\0setYRotation\0setZRotation\0"
-    "setMyPrueba\0linea_button_pressed_slot\0"
+    "MyGLWidget\0linea_button_pressed_signal\0"
+    "\0press\0progress_bar_signal\0progress\0"
+    "spin_signal\0value\0linea_button_pressed_slot\0"
     "triangulo_button_pressed_slot\0"
     "rectangulo_button_pressed_slot\0"
     "elipse_button_pressed_slot\0"
-    "done_button_pressed_slot\0red_slider_slot\0"
-    "number\0blue_slider_slot\0green_slider_slot\0"
-    "add_button_pressed_slot\0open_file_slot\0"
-    "progress_bar_slot"
+    "done_button_pressed_slot\0spin_slot\0"
+    "red_slider_slot\0number\0blue_slider_slot\0"
+    "green_slider_slot\0add_button_pressed_slot\0"
+    "open_file_slot\0progress_bar_slot"
 };
 #undef QT_MOC_LITERAL
 
@@ -80,62 +70,50 @@ static const uint qt_meta_data_MyGLWidget[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      21,   14, // methods
+      15,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       6,       // signalCount
+       3,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    1,  119,    2, 0x06 /* Public */,
-       4,    1,  122,    2, 0x06 /* Public */,
-       5,    1,  125,    2, 0x06 /* Public */,
-       6,    1,  128,    2, 0x06 /* Public */,
-       8,    1,  131,    2, 0x06 /* Public */,
-      10,    1,  134,    2, 0x06 /* Public */,
+       1,    1,   89,    2, 0x06 /* Public */,
+       4,    1,   92,    2, 0x06 /* Public */,
+       6,    1,   95,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-      12,    1,  137,    2, 0x0a /* Public */,
-      13,    1,  140,    2, 0x0a /* Public */,
-      14,    1,  143,    2, 0x0a /* Public */,
-      15,    1,  146,    2, 0x0a /* Public */,
-      16,    1,  149,    2, 0x0a /* Public */,
-      17,    1,  152,    2, 0x0a /* Public */,
-      18,    1,  155,    2, 0x0a /* Public */,
-      19,    1,  158,    2, 0x0a /* Public */,
-      20,    0,  161,    2, 0x0a /* Public */,
-      21,    1,  162,    2, 0x0a /* Public */,
-      23,    1,  165,    2, 0x0a /* Public */,
-      24,    1,  168,    2, 0x0a /* Public */,
-      25,    0,  171,    2, 0x0a /* Public */,
-      26,    0,  172,    2, 0x0a /* Public */,
-      27,    1,  173,    2, 0x0a /* Public */,
+       8,    1,   98,    2, 0x0a /* Public */,
+       9,    1,  101,    2, 0x0a /* Public */,
+      10,    1,  104,    2, 0x0a /* Public */,
+      11,    1,  107,    2, 0x0a /* Public */,
+      12,    0,  110,    2, 0x0a /* Public */,
+      13,    1,  111,    2, 0x0a /* Public */,
+      14,    1,  114,    2, 0x0a /* Public */,
+      16,    1,  117,    2, 0x0a /* Public */,
+      17,    1,  120,    2, 0x0a /* Public */,
+      18,    0,  123,    2, 0x0a /* Public */,
+      19,    0,  124,    2, 0x0a /* Public */,
+      20,    1,  125,    2, 0x0a /* Public */,
 
  // signals: parameters
-    QMetaType::Void, QMetaType::Int,    3,
-    QMetaType::Void, QMetaType::Int,    3,
-    QMetaType::Void, QMetaType::Int,    3,
+    QMetaType::Void, QMetaType::Bool,    3,
+    QMetaType::Void, QMetaType::Int,    5,
     QMetaType::Void, QMetaType::Int,    7,
-    QMetaType::Void, QMetaType::Bool,    9,
-    QMetaType::Void, QMetaType::Int,   11,
 
  // slots: parameters
-    QMetaType::Void, QMetaType::Int,    3,
-    QMetaType::Void, QMetaType::Int,    3,
-    QMetaType::Void, QMetaType::Int,    3,
+    QMetaType::Void, QMetaType::Bool,    3,
+    QMetaType::Void, QMetaType::Bool,    3,
+    QMetaType::Void, QMetaType::Bool,    3,
+    QMetaType::Void, QMetaType::Bool,    3,
+    QMetaType::Void,
     QMetaType::Void, QMetaType::Int,    7,
-    QMetaType::Void, QMetaType::Bool,    9,
-    QMetaType::Void, QMetaType::Bool,    9,
-    QMetaType::Void, QMetaType::Bool,    9,
-    QMetaType::Void, QMetaType::Bool,    9,
-    QMetaType::Void,
-    QMetaType::Void, QMetaType::Int,   22,
-    QMetaType::Void, QMetaType::Int,   22,
-    QMetaType::Void, QMetaType::Int,   22,
+    QMetaType::Void, QMetaType::Int,   15,
+    QMetaType::Void, QMetaType::Int,   15,
+    QMetaType::Void, QMetaType::Int,   15,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::Int,   11,
+    QMetaType::Void, QMetaType::Int,    5,
 
        0        // eod
 };
@@ -145,66 +123,42 @@ void MyGLWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
     if (_c == QMetaObject::InvokeMetaMethod) {
         MyGLWidget *_t = static_cast<MyGLWidget *>(_o);
         switch (_id) {
-        case 0: _t->xRotationChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 1: _t->yRotationChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 2: _t->zRotationChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 3: _t->pruebaChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 4: _t->linea_button_pressed_signal((*reinterpret_cast< bool(*)>(_a[1]))); break;
-        case 5: _t->progress_bar_signal((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 6: _t->setXRotation((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 7: _t->setYRotation((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 8: _t->setZRotation((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 9: _t->setMyPrueba((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 10: _t->linea_button_pressed_slot((*reinterpret_cast< bool(*)>(_a[1]))); break;
-        case 11: _t->triangulo_button_pressed_slot((*reinterpret_cast< bool(*)>(_a[1]))); break;
-        case 12: _t->rectangulo_button_pressed_slot((*reinterpret_cast< bool(*)>(_a[1]))); break;
-        case 13: _t->elipse_button_pressed_slot((*reinterpret_cast< bool(*)>(_a[1]))); break;
-        case 14: _t->done_button_pressed_slot(); break;
-        case 15: _t->red_slider_slot((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 16: _t->blue_slider_slot((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 17: _t->green_slider_slot((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 18: _t->add_button_pressed_slot(); break;
-        case 19: _t->open_file_slot(); break;
-        case 20: _t->progress_bar_slot((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 0: _t->linea_button_pressed_signal((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 1: _t->progress_bar_signal((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 2: _t->spin_signal((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 3: _t->linea_button_pressed_slot((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 4: _t->triangulo_button_pressed_slot((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 5: _t->rectangulo_button_pressed_slot((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 6: _t->elipse_button_pressed_slot((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 7: _t->done_button_pressed_slot(); break;
+        case 8: _t->spin_slot((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 9: _t->red_slider_slot((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 10: _t->blue_slider_slot((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 11: _t->green_slider_slot((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 12: _t->add_button_pressed_slot(); break;
+        case 13: _t->open_file_slot(); break;
+        case 14: _t->progress_bar_slot((*reinterpret_cast< int(*)>(_a[1]))); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
         int *result = reinterpret_cast<int *>(_a[0]);
         void **func = reinterpret_cast<void **>(_a[1]);
         {
-            typedef void (MyGLWidget::*_t)(int );
-            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MyGLWidget::xRotationChanged)) {
+            typedef void (MyGLWidget::*_t)(bool );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MyGLWidget::linea_button_pressed_signal)) {
                 *result = 0;
             }
         }
         {
             typedef void (MyGLWidget::*_t)(int );
-            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MyGLWidget::yRotationChanged)) {
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MyGLWidget::progress_bar_signal)) {
                 *result = 1;
             }
         }
         {
             typedef void (MyGLWidget::*_t)(int );
-            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MyGLWidget::zRotationChanged)) {
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MyGLWidget::spin_signal)) {
                 *result = 2;
-            }
-        }
-        {
-            typedef void (MyGLWidget::*_t)(int );
-            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MyGLWidget::pruebaChanged)) {
-                *result = 3;
-            }
-        }
-        {
-            typedef void (MyGLWidget::*_t)(bool );
-            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MyGLWidget::linea_button_pressed_signal)) {
-                *result = 4;
-            }
-        }
-        {
-            typedef void (MyGLWidget::*_t)(int );
-            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MyGLWidget::progress_bar_signal)) {
-                *result = 5;
             }
         }
     }
@@ -235,56 +189,35 @@ int MyGLWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 21)
+        if (_id < 15)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 21;
+        _id -= 15;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 21)
+        if (_id < 15)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 21;
+        _id -= 15;
     }
     return _id;
 }
 
 // SIGNAL 0
-void MyGLWidget::xRotationChanged(int _t1)
+void MyGLWidget::linea_button_pressed_signal(bool _t1)
 {
     void *_a[] = { 0, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
     QMetaObject::activate(this, &staticMetaObject, 0, _a);
 }
 
 // SIGNAL 1
-void MyGLWidget::yRotationChanged(int _t1)
+void MyGLWidget::progress_bar_signal(int _t1)
 {
     void *_a[] = { 0, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
     QMetaObject::activate(this, &staticMetaObject, 1, _a);
 }
 
 // SIGNAL 2
-void MyGLWidget::zRotationChanged(int _t1)
+void MyGLWidget::spin_signal(int _t1)
 {
     void *_a[] = { 0, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
     QMetaObject::activate(this, &staticMetaObject, 2, _a);
-}
-
-// SIGNAL 3
-void MyGLWidget::pruebaChanged(int _t1)
-{
-    void *_a[] = { 0, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
-    QMetaObject::activate(this, &staticMetaObject, 3, _a);
-}
-
-// SIGNAL 4
-void MyGLWidget::linea_button_pressed_signal(bool _t1)
-{
-    void *_a[] = { 0, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
-    QMetaObject::activate(this, &staticMetaObject, 4, _a);
-}
-
-// SIGNAL 5
-void MyGLWidget::progress_bar_signal(int _t1)
-{
-    void *_a[] = { 0, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
-    QMetaObject::activate(this, &staticMetaObject, 5, _a);
 }
 QT_END_MOC_NAMESPACE
