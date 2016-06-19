c5345b491177f5a93f2c6b5ace3c00acf0daa065
belgrades <fcremaldc@gmail.com>
2016-06-19 12:04:54 -0400

    Adding comments to linea.cpp

--- linea.cpp
+++ linea.cpp
@@ -15,16 +15,21 @@ linea::linea(QVector<QVector3D*> opengl, QVector<QVector3D*> pixel, QVector3D co
 
 void linea::dibujar_figura()
 {
+    // Size of control points
     glPointSize(5.0);
     if(done)
         glColor3f(0.0,1.0,0.0);
     else
         glColor3f(1.0,1.0,0.0);
+    
+
+    // Drawing control points
     glBegin(GL_POINTS);
         for(int i=0;i<puntos_control_opengl.size();i++){
             glVertex3f(puntos_control_opengl[i]->x(),puntos_control_opengl[i]->y(),0);
         }
     glEnd();
+
 
     glColor3f((GLfloat)color.x(), (GLfloat)color.y(), (GLfloat)color.z());
     glBegin(GL_LINES);
